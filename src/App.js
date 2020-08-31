751c1a4a4f5b8538c787f80651f9a1740fbe8ca1
Nishita V <nishitavaddem@gmail.com>
2020-08-31 19:07:59 +0530

    integrated the Alan button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web'
+
+const alanKey = '3194abb58c2dd4ed6af2d98bce1343782e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if (command === 'testCommand') {
+          alert('This code was executed')
+        }
+      },
+
+    })
+  }, [])
+
   return (
     <div>
       <h1>Alan AI News Application</h1>
