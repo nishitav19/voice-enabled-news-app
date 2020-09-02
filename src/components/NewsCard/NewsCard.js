3e3b5bcc8c2d791e0cc4505aaef7cc8401a9cda9
Nishita V <nishitavaddem@gmail.com>
2020-09-02 19:27:22 +0530

    Designing individual card components

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Card, CardActions, CardMedia, Button, CardActionArea, CardContent, Typography } from '@material-ui/core'
 
-const NewsCard = () => {
+const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, i }) => {
     return (
-        <div>
-            News Card
-        </div>
+        <Card>
+            <CardActionArea>
+                <CardMedia image={urlToImage}/>
+                <div>
+                    <Typography variant="body2" color="textSecondary" component="h2">
+                        {(new Date(publishedAt)).toDateString()}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="h2">
+                        {source.name}
+                    </Typography>
+                </div>
+                <Typography gutterBottom variant="h5"></Typography>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary"> Learn More </Button>
+                <Typography variant="h5" color="textSecondary">{ i + 1 }</Typography>
+            </CardActions>
+        </Card>
     )
 }
 
